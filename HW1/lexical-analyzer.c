5a6defdac26ed8f094696f71e6a841e24b37d29b
jaejung <ljj0512@gmail.com>
2022-09-18 05:14:20 +0000

    MODIFY HW1

--- HW1/lexical-analyzer.c
+++ HW1/lexical-analyzer.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+typedef struct Node* nodePointer;
+typedef struct Node{
+    int index;
+    char* info;
+    nodePointer next;
+}Node;
+
+nodePointer symbol_table_head;
+nodePointer string_table_head;
+
 int main(){
-    printf("lexical analyzer\n");
-    printf("dlkfsl\n");
+    char c;
+    while((c=getchar())!=-1){
+        printf("%c",c);
+    }
+    printf("\n");
     return 0;
 }
