90a82c0b3266fc6b5fe1a357fe1083fd98cab55c
jaejung <ljj0512@gmail.com>
2022-09-29 19:07:21 +0900

    ADD automata-base-code.c

---
+++ automata-base-code.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int state = 0;
+    int i = 0;
+    char input[30] = {"abbb"};   //ERROR
+    // char input = {"aabb"};
+    switch (state)
+    {
+        case 0:
+            if(input[i]=='a') { state = 1; i++; }
+            else if(input[i]=='b') { state = 0; i++;}
+            else {state = -1; i++;}
+            break;
+
+        case 1:
+            if(input[i]=='a') { state = 1; i++; }
+            else if(input[i]=='b') { state = 0; i++;}
+            else {state = -1; i++;}
+            break;
+        
+        default:
+            break;
+    }
+    // while(1){
+
+    // }
+    return 0;
+}
